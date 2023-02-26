8e0c7f5fc9c8ab15caa8cca2da5beae6afe22efc
Alin Florin <alin@qoffice.ro>
2023-02-26 20:32:46 +0200

    -

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,39 @@ import ChatBot from "sydney-ai";
 
 config();
 
-const chatBot = new ChatBot(process.env.BINGTOKEN || "wrong");
-
 (async () => {
   try {
     const app = express();
     app.use(express.json());
 
     app.post(`/api/chat/ask`, async (req, res) => {
-      try {
-        const response = await chatBot.ask(
-          req.body.question,
-          req.body.conversationId || undefined
-        );
+      const chatBot = new ChatBot(process.env.BINGTOKEN || "wrong");
+        try {
+          const response = await chatBot.ask(
+            req.body.question,
+            req.body.conversationId || undefined
+          );
 
-        res.send({
-          reply: response,
-          conversationId: req.body.conversationId,
-        });
-      } catch (err) {
-        res.status(500).send("Error!");
-        console.error(err);
-      }
+          res.send({
+            reply: response,
+            conversationId: req.body.conversationId,
+          });
+        } catch (err) {
+          res.status(500).send("Error!");
+          console.error(err);
+        } finally {
+          if (chatBot.conversations) {
+            chatBot.conversations.forEach(c => { 
+              if (c.isConnected) {
+                try {
+                  c.disconnect().then();
+                } catch (err2) {
+                  console.error(err2);
+                }
+              }
+            });
+          }
+        }
     });
 
     app.listen(3000, () => {
