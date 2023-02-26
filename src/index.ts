3a4691ee20df3adf805fccf4bbe3e9c489175019
Alin Florin <alin@qoffice.ro>
2023-02-27 00:49:51 +0200

    -

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,49 @@
 import express from "express";
 import { config } from "dotenv";
 import ChatBot from "sydney-ai";
+import "express-async-errors";
 
 config();
+
+const chatBot = new ChatBot(process.env.BINGTOKEN || "wrong");
 
 (async () => {
   try {
     const app = express();
     app.use(express.json());
 
-    app.post(`/api/chat/ask`, async (req, res) => {
-      const chatBot = new ChatBot(process.env.BINGTOKEN || "wrong");
-        try {
-          const response = await chatBot.ask(
-            req.body.question,
-            req.body.conversationId || undefined
-          );
+    app.post(`/api/ask`, async (req, res) => {
+      const reply = await chatBot.ask(
+        req.body.question,
+        req.body.conversationId
+      );
+      res.send({reply: reply});
+    });
 
-          res.send({
-            reply: response,
-            conversationId: req.body.conversationId,
-          });
-        } catch (err) {
-          res.status(500).send("Error!");
-          console.error(err);
-        } finally {
-          if (chatBot.conversations) {
-            chatBot.conversations.forEach(c => { 
-              if (c.isConnected) {
-                try {
-                  c.disconnect().then();
-                } catch (err2) {
-                  console.error(err2);
-                }
-              }
-            });
-          }
-        }
+    app.post(`/api/askStream`, async (req, res) => {
+      try {
+        res.setHeader("Cache-Control", "no-cache");
+        res.setHeader("Content-Type", "text/event-stream");
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Connection", "keep-alive");
+        res.flushHeaders();
+
+        res.on("close", () => {
+          res.end();
+        });
+
+        await chatBot.askStream(
+          (d) => {
+            res.write(`data: ${JSON.stringify({ reply: d })}\n\n`);
+          },
+          req.body.question,
+          req.body.conversationId
+        );
+      } catch (err) {
+        console.error(err);
+      } finally {
+        res.end();
+      }
     });
 
     app.listen(3000, () => {
